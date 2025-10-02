0c47575f4f5d22c4bfe6287dd3cd75706e28f501
Shivam Kumar <119784386+Shivambhagatt@users.noreply.github.com>
2025-10-02 13:00:32 +0530

    Delete projects/buttons/projects/buttons/shivambhagatt/projects/buttons/shivambhagatt directory

--- projects/buttons/projects/buttons/shivambhagatt/projects/buttons/shivambhagatt/js/main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const btn = document.getElementById("hacktober-btn");
-
-  btn.addEventListener("click", () => {
-    alert("🎉 Thanks for clicking my Hacktoberfest Button!");
-  });
-});
